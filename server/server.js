39dd24759e4ffbbf019f746b415ad95348342d4c
jmmalappil <jmalappil@gmail.com>
2024-10-28 15:47:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,11 +25,11 @@ app.get('/api', (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Catch-all handler for any request not handled by other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
